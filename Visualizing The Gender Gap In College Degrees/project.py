a30ce4cc7d2b6a904e1a74c16fa9c084800f8e97
Ardhimas Kamdani <kamdani.af@gmail.com>
2018-02-18 16:58:16 -0600

    initialiazed and completed q1 on Visualizing the Gender Gap in College Degrees guided project

---
+++ Visualizing The Gender Gap In College Degrees/project.py
@@ -0,0 +1,77 @@
+%matplotlib inline
+import pandas as pd
+import matplotlib.pyplot as plt
+# Generate a 6 row by 3 column grid of subplots.
+women_degrees = pd.read_csv('/Users/ardy/Documents/dataquest/Visualizing The Gender Gap In College Degrees/percent-bachelors-degrees-women-usa.csv')
+cb_dark_blue = (0/255,107/255,164/255)
+cb_orange = (255/255, 128/255, 14/255)
+stem_cats = ['Engineering', 'Computer Science', 'Psychology', 'Biology', 'Physical Sciences', 'Math and Statistics']
+lib_arts_cats = ['Foreign Languages', 'English', 'Communications and Journalism', 'Art and Performance', 'Social Sciences and History']
+other_cats = ['Health Professions', 'Public Administration', 'Education', 'Agriculture','Business', 'Architecture']
+
+fig = plt.figure(figsize=(16, 20))
+
+# In the first column:
+# Generate line charts for both male and female percentages for every degree in stem_cats.
+# Add text annotations for Women and Men in the topmost and bottommost plots.
+for sp in range(0,18,3):
+    cat_index = int(sp/3)
+    ax = fig.add_subplot(6,3,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[stem_cats[cat_index]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[stem_cats[cat_index]], c=cb_orange, label='Men', linewidth=3)
+    for key,spine in ax.spines.items():
+        spine.set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(stem_cats[cat_index])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+
+    if cat_index == 0:
+        ax.text(2003, 85, 'Women')
+        ax.text(2005, 10, 'Men')
+    elif cat_index == 5:
+        ax.text(2005, 87, 'Men')
+        ax.text(2003, 7, 'Women')
+
+# In the second column:
+# Generate line charts for both male and female percentages for every degree in lib_arts_cats.
+# Add text annotations for Women and Men for only the topmost plot (since the lines overlap at the end in the bottommost plot).
+for sp in range(1,16,3):
+    cat_index = int((sp-1)/3)
+    ax = fig.add_subplot(6,3,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[lib_arts_cats[cat_index]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[lib_arts_cats[cat_index]], c=cb_orange, label='Men', linewidth=3)
+    for key,spine in ax.spines.items():
+        spine.set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(lib_arts_cats[cat_index])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+
+    if cat_index == 0:
+        ax.text(2003, 78, 'Women')
+        ax.text(2005, 18, 'Men')
+
+# In the third column:
+# Generate line charts for both male and female percentages for every degree in other_cats.
+# Add text annotations for Women and Men in the topmost and bottommost plots.
+for sp in range(2,20,3):
+    cat_index = int((sp-2)/3)
+    ax = fig.add_subplot(6,3,sp+1)
+    ax.plot(women_degrees['Year'], women_degrees[other_cats[cat_index]], c=cb_dark_blue, label='Women', linewidth=3)
+    ax.plot(women_degrees['Year'], 100-women_degrees[other_cats[cat_index]], c=cb_orange, label='Men', linewidth=3)
+    for key,spine in ax.spines.items():
+        spine.set_visible(False)
+    ax.set_xlim(1968, 2011)
+    ax.set_ylim(0,100)
+    ax.set_title(other_cats[cat_index])
+    ax.tick_params(bottom="off", top="off", left="off", right="off")
+
+    if cat_index == 0:
+        ax.text(2003, 90, 'Women')
+        ax.text(2005, 5, 'Men')
+    elif cat_index == 5:
+        ax.text(2005, 62, 'Men')
+        ax.text(2003, 30, 'Women')
+
+plt.show()
